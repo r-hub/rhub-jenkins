fda8228af67efb1cda2648b077a7fa3235ab302c
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-08 12:23:32 +0000

    Install swarm plugin at startup

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,7 @@ MAINTAINER "r-hub admin" admin@r-hub.io
 
 USER jenkins
 
+ENV JENKINS_PLUGINS "swarm"
+
 COPY docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
 ENTRYPOINT ["/usr/local/bin/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -25,6 +25,52 @@ instance.save()
 _EOF_
 fi
 
+if [ -n "${JENKINS_PLUGINS}" ]; then
+  if [ ! -d "${JENKINS_HOME}/init.groovy.d" ]; then
+    mkdir ${JENKINS_HOME}/init.groovy.d
+  fi
+  jenkins_plugins=${JENKINS_PLUGINS}
+  cat > ${JENKINS_HOME}/init.groovy.d/loadPlugins.groovy <<_EOF_
+  import jenkins.model.*
+  import java.util.logging.Logger
+
+  def logger = Logger.getLogger("")
+  def installed = false
+  def initialized = false
+
+  def pluginParameter="${jenkins_plugins}"
+  def plugins = pluginParameter.split()
+  logger.info("" + plugins)
+  def instance = Jenkins.getInstance()
+  def pm = instance.getPluginManager()
+  def uc = instance.getUpdateCenter()
+  uc.updateAllSites()
+
+  plugins.each {
+    logger.info("Checking " + it)
+    if (!pm.getPlugin(it)) {
+      logger.info("Looking UpdateCenter for " + it)
+      if (!initialized) {
+        uc.updateAllSites()
+        initialized = true
+      }
+      def plugin = uc.getPlugin(it)
+      if (plugin) {
+        logger.info("Installing " + it)
+        plugin.deploy()
+        installed = true
+      }
+    }
+  }
+
+  if (installed) {
+    logger.info("Plugins installed, initializing a restart!")
+    instance.save()
+    instance.doSafeRestart()
+  }
+_EOF_
+fi
+
 unset JENKINS_ADMIN_USER
 unset JENKINS_ADMIN_PASSWORD
 
