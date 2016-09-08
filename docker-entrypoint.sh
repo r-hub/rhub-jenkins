fdfb3ee5f551bfc691c771d15cb364f4a97d10ee
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-08 14:19:16 +0100

    Command line argument to set jenkins to UTF-8

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -154,6 +154,7 @@ cat > ${JENKINS_HOME}/init.groovy.d/setEnvVars.groovy <<_EOF_
   }
 
   envVars.put("PS4", "+R-HUB-R-HUB-R-HUB")
+  envVars.put("JAVA_OPTS", "-Dfile.encoding=UTF-8")
 
   instance.save()
 _EOF_
